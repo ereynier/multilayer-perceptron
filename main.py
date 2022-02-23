a361e1ad63f4c3308a41d6032eb65976e1f3b9b6
ereynier <ereynier@student.42lyon.fr>
2022-02-23 15:07:26 +0100

    rename columns

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import pandas as pd
 
+def rename_col(data):
+    data.rename(columns={0 : "ID", 
+                         1 : "Diagnosis",
+                         2 : "radius",
+                         3 : "texture",
+                         4 : "perimeter",
+                         5 : "area",
+                         6 : "smoothness",
+                         7 : "compactness",
+                         8 : "concavity",
+                         9 : "concave points",
+                         10 : "symmetry",
+                         11 : "fractal"}, inplace=True)
+    data.set_index('ID', inplace=True)
+    suffix = [i + "_SE" for i in list(data.columns[1:11])]
+    data.rename(columns=dict(zip(data.columns[11:22], suffix)), inplace=True)
+    suffix = [i + "_Worst" for i in list(data.columns[1:11])]
+    data.rename(columns=dict(zip(data.columns[21:33], suffix)), inplace=True)
+    return (data)
 
 def main():
     try:
         data = pd.read_csv("data/data.csv", header=None)
-        print(data.head())
     except:
         print("Can't read data.csv")
         return
+    data=rename_col(data)
+    print(data)
 
 
 if __name__ == "__main__":
