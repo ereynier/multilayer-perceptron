bee3d599276093b103745ba74a3f92db6656c45c
ereynier <ereynier@student.42lyon.fr>
2022-03-04 16:51:27 +0100

    split plotData and main

--- plotData.py
+++ plotData.py
@@ -7,7 +7,6 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 import yaml
 import sys
-from sklearn.metrics import accuracy_score
 
 def renameCol(data):
     data.rename(columns={0 : "ID", 
@@ -29,16 +28,23 @@ def renameCol(data):
     data.rename(columns=dict(zip(data.columns[21:33], suffix)), inplace=True)
     return (data)
 
-def selectData(df):
-    with open('config.yml', "r") as f:
-        config = yaml.safe_load(f)
-    #USE CONFIG FILE
-    X = df[config["features"]]
-    return (X)
+def plotData(df):
+    print(df.describe())
+    fig = plt.figure(figsize=(30,30))
+    sns.heatmap(df.corr())
+    plt.savefig("fig/corr_heatmap.png")
 
-def normalize(df):
-    normalized_df = (df - df.mean()) / df.std()
-    return (normalized_df)
+    print(df["Diagnosis"].value_counts(normalize=True))
+
+    i = 1
+    fig = plt.figure(figsize=(30,30))
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    for col in df.select_dtypes(float):
+        ax = fig.add_subplot(6, 6, i)
+        i += 1
+        sns.histplot(x=df[col], hue=df["Diagnosis"], kde=True, ax=ax)
+    plt.savefig(f'fig/dist.png')
+
 
 def main():
     try:
@@ -49,44 +55,7 @@ def main():
     data=renameCol(data)
     df = data.copy()
 
-    enc = OneHotEncoder(sparse=False)
-
-    y = enc.fit_transform(df[["Diagnosis"]].values)
-
-    X = selectData(df)
-
-    X = normalize(X)
-
-    print(X.head)
-
-    X = X.to_numpy()
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-    #transpose X y
-    X_test = X_test.T
-    X_train = X_train.T
-    y_train = y_train.T
-    y_test = y_test.T
-
-
-    network = NeuralNetwork([X_train.shape[0], 32, 32, y_train.shape[0]])
-    
-    network.fit_(X_train, y_train, epoch=80, batch_size=500, plot=True)
-
-    y_pred = network.predict_(X_test)
-
-    print(f'Loss: {network.cross_entropy_(network.activations["A" + str(network.n_layers)] ,y_pred):.4f}')
-
-    #y_test proportion de bonne réponses
-    y_pred = enc.inverse_transform(y_pred.T)
-    y_test = enc.inverse_transform(y_test.T)
-    c = 0
-    for i in range(len(y_pred)):
-        if y_pred[i] == y_test[i]:
-            c += 1
-    print(f'Good prediction: {(c / len(y_pred))*100:.2f}%')
-    print(f'Accuracy: {accuracy_score(y_test.flatten(), y_pred.flatten())}')
+    plotData(df)
 
 
 if __name__ == "__main__":
